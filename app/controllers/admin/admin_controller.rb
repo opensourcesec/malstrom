21559329ff5dee839e821e5e042c5aefbf2da7b7
root <malhunters@gmail.com>
2014-05-22 16:15:09 -0700

    Got dropdown sign in functional, but doesn't recognize valid creds.

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,6 +1,7 @@
 class Admin::AdminController < ApplicationController
   layout "admin"
   before_filter :authenticate_user!
+
   def current_ability
     @current_ability ||= AdminAbility.new(current_user)
   end
