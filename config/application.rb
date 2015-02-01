cdfb7582b79b8a0bc35a5cda15fe9547fb2aeb66
Bobby <y0xda@Rivendell.local>
2015-01-31 20:10:20 -0800

    Added delayed job to application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MSSD
+config.active_job.queue_adapter = :delayed_job
+
+module Malstrom
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
